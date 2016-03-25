e1c0f5ad248b5f9e0e054ca141d4eab9e50120a8
admin <897240781@qq.com>
2016-03-25 22:25:13 +0800

    Initial Commit

---
+++ SeptBaisi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeptBaisi
+//
+//  Created by admin on 16/3/25.
+//  Copyright © 2016年 Sept. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
